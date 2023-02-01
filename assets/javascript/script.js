d90b2837ee4cacc35e407cf5407fdee4d63ec49b
Stefan Marinov <stefan_marinov@rogers.com>
2023-01-31 19:42:54 -0500

    added form validation for space and mars retrieval; added logo image

---
+++ assets/javascript/script.js
@@ -0,0 +1,96 @@
+// sets up the max attribute of the days to current date
+$(document).ready(function () {
+    clearControlPanel();
+
+});
+
+//helper variables
+let spaceStartDateOK = false;
+let spaceEndDateOK = false;
+let roverDateOK = false;
+let roverCameraOK = false;
+
+//click listeners to ensure that we have valid date range
+// upon change:
+//  - sets the min attribute of end date to the startDate
+//  - if there is already an end date that is prior to the start date, the end date is changed
+$("#spaceStartDate").on("change", function (event) {
+    $("#spaceEndDate").attr("min", dayjs($("#spaceStartDate").val()).format("YYYY-MM-DD"));
+    if (dayjs($("#spaceStartDate").val()) > dayjs($("#spaceEndDate").val())) {
+        $("#spaceEndDate").val($("#spaceStartDate").val());
+    }
+
+});
+// upon change:
+//   - sets the max attribute of start date equal to end date
+$("#spaceEndDate").on("change", function (event) {
+    $("#spaceStartDate").attr("max", dayjs($("#spaceEndDate").val()).format("YYYY-MM-DD"));
+});
+
+// event listener on the Control Panel; the only valid selections are:
+//  - the accordeon clicks
+//  - the buttons 
+$("#missionControlPanel").on("click", function (event) {
+
+    event.stopPropagation()
+    if (event.target.matches("a")) {
+        clearControlPanel();
+    }
+    if (event.target.matches("#spaceMissionStart")) {
+        // these framework listeners are required to prevent further action on invalid input
+        startSpaceMission();
+    }
+    if (event.target.matches("#roverMissionStart")) {
+        // these framework listeners are required to prevent further action on invalid input
+        startRoverMission();
+    }
+});
+// removes any data from the control panel as the users flips between options
+// NOTE: if there's already an error, the error messaging will remain
+function clearControlPanel() {
+    $("#spaceStartDate").val("");
+    $("#spaceStartDate").attr("max", dayjs().format("YYYY-MM-DD"));
+    $("#spaceStartDate").removeAttr("min");
+    $("#spaceEndDate").val("");
+    $("#spaceEndDate").attr("max", dayjs().format("YYYY-MM-DD"));
+    $("#spaceEndDate").removeAttr("min");
+    $("#roverDate").val("");
+    $("#roverCamera").val("");
+    spaceStartDateOK = false;
+    spaceEndDateOK = false;
+    roverDateOK = false;
+    roverCameraOK = false;
+
+}
+// event listeners for good input and sets the helper variables to true
+$("#spaceStartDate").on("valid.zf.abide", function (ev, el) {
+    spaceStartDateOK = true;
+});
+$("#spaceEndDate").on("valid.zf.abide", function (ev, el) {
+    spaceEndDateOK = true;
+});
+$("#roverDate").on("valid.zf.abide", function (ev, el) {
+    roverDateOK = true;
+});
+$("#roverCamera").on("valid.zf.abide", function (ev, el) {
+    roverCameraOK = true;
+});
+
+// starting the space mission; no action takes place while we have invalid inputs
+function startSpaceMission() {
+    if (spaceStartDateOK && spaceEndDateOK) {
+        alert("fetch and display SPACE images");
+    } else {
+        alert("must NOT fetch and display SPACE images");
+
+    }
+}
+// starting the space mission; no action takes place while we have invalid inputs
+function startRoverMission() {
+    if (roverDateOK && roverCameraOK) {
+        alert("fetch and display ROVER images");
+    } else {
+        alert("must NOT fetch and display ROVER images");
+
+    }
+}
